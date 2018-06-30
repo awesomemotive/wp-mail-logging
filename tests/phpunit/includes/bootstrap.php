7a5ec671f2d879e0e62041863bc20240416959cb
Christian Zöller <noex_home@yahoo.de>
2018-06-30 16:33:28 +0200

    Try old bootstrap mechanism

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -17,5 +17,31 @@ echo 'Tests folder: ' . $loader->locate_wp_tests_config() . PHP_EOL;
 $loader->add_plugin( 'wp-mail-logging/wp-mail-logging.php' );
 $loader->load_wordpress();
 
-require_once( dirname( __FILE__ ) . '/../../../wp-mail-logging.php' );
+if ( ! $_tests_dir ) {
+    $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+echo 'Tests folder: ' . $_tests_dir . PHP_EOL;
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+    echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+    exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+    $path = dirname(dirname( dirname( dirname( __FILE__ ) ) ) ) . '/wp-mail-logging.php';
+    echo "Plugin path: " . $path;
+    require $path;
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
+
 // EOF
