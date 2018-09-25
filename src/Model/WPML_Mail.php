3bafbc5294a1e37c8bc6a6b3a58c360562a8a7e8
Christian Zöller <noex_home@yahoo.de>
2018-09-25 19:01:33 +0200

    Added comments for code completion for __call setter and getter

--- src/Model/WPML_Mail.php
+++ src/Model/WPML_Mail.php
@@ -12,16 +12,35 @@ if(!defined( 'ABSPATH' )) exit;
  * WPML Mail model.
  * @since 1.6.0
  * @author No3x
+ * @method integer get_mail_id()
+ * @method set_mail_id(integer $id)
+ * @method string get_timestamp()
+ * @method set_timestamp(string $timestamp)
+ * @method string get_host()
+ * @method set_host(string $host)
+ * @method string get_receiver()
+ * @method set_receiver(string $receiver)
+ * @method string get_subject()
+ * @method set_subject(string $subject)
+ * @method string get_message()
+ * @method set_message(string $message)
+ * @method string get_headers()
+ * @method set_headers(string $headers)
+ * @method string get_attachments()
+ * @method set_attachments(string $attachments)
+ * @method string get_error()
+ * @method set_error(string|string[] $error)
+ * @method string get_plugin_version()
+ * @method set_plugin_version(string $plugin_version)
  */
-class WPML_Mail extends BaseModel
-{
+class WPML_Mail extends BaseModel {
     /**
      * @var integer
      */
     protected $mail_id;
 
     /**
-     * @var DateTime
+     * @var string
      */
     protected $timestamp;
 
