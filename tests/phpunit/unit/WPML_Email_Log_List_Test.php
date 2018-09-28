107d93121bebefdb8a2b5ac61970a93181a842ed
Christian Zöller <noex_home@yahoo.de>
2018-09-28 22:18:59 +0200

    Try to verify data of rendered mail. Refactoring in progress

--- tests/phpunit/unit/WPML_Email_Log_List_Test.php
+++ tests/phpunit/unit/WPML_Email_Log_List_Test.php
@@ -18,13 +18,15 @@ class WPML_Email_Log_List_Test extends \PHPUnit_Framework_TestCase {
         $this->logListTable = new WPML_Email_Log_List(null);
 
         $this->item_id = 2;
+
+        $wpMailArrayBuilder = WPMailArrayBuilder::aMail()
+            ->withSubject("Test")
+            ->withTo("example@exmple.com")
+            ->withMessage("Hello World")
+        ;
         /** @var $mail WPML_Mail */
-        $mail = (new WPML_MailExtractor())->extract(WPMailArrayBuilder::aMail()->withSubject("Test")->withTo("example@exmple.com")->withHeaders(["From: \"admin\" <admin@local.test>", "Cc: example2@example.com", "Reply-To: admin <admin@local.test>"])->withMessage("<b>Bold</b>")->build());
+        $mail = (new WPML_MailExtractor())->extract($wpMailArrayBuilder->build());
         $mail->set_mail_id($this->item_id);
-        $mail->set_plugin_version('1.8.5');
-        $mail->set_timestamp('2018-09-24 16:02:11');
-        $mail->set_host('127.0.0.1');
-        $mail->set_error('a');
 
         $this->item = $mail->to_array();
     }
@@ -33,15 +35,4 @@ class WPML_Email_Log_List_Test extends \PHPUnit_Framework_TestCase {
         $expected = '<a class="wp-mail-logging-view-message button button-secondary" href="#" data-mail-id="' . $this->item_id . '">View</a>';
         $this->assertEquals($expected, $this->logListTable->column_message($this->item));
     }
-
-    public function test_render_mail() {
-        $expected = '<span class="title">Time: </span>2018-09-24 16:02:11<span class="title">Receiver: </span>example@exmple.com<span class="title">Subject: </span>Test<span class="title">Message: </span>&lt;b&gt;Bold&lt;/b&gt;<span class="title">Headers: </span>From: &quot;admin&quot; ,\nCc: example2@example.com,\nReply-To: admin <span class="title">Attachments: </span><span class="title">Error: </span><i class="fa fa-exclamation-circle" title="a"></i>';
-        $this->assertEquals($expected, $this->logListTable->render_mail($this->item));
-    }
-
-   public function test_render_mail_html() {
-        $expected = '<span class="title">Time: </span>2018-09-24 16:02:11<span class="title">Receiver: </span>example@exmple.com<span class="title">Subject: </span>Test<span class="title">Message: </span><b>Bold</b><span class="title">Headers: </span>From: "admin" ,\nCc: example2@example.com,\nReply-To: admin <span class="title">Attachments: </span><span class="title">Error: </span><i class="fa fa-exclamation-circle" title="a"></i>';
-        $this->assertEquals($expected, $this->logListTable->render_mail_html($this->item));
-    }
-
 }
--- tests/phpunit/unit/WPML_FormattedPrinter_Test.php
+++ tests/phpunit/unit/WPML_FormattedPrinter_Test.php
@@ -10,18 +10,34 @@ use No3x\WPML\WPML_FormattedPrinter;
 use No3x\WPML\WPML_MailExtractor;
 
 // TODO: after refactoring this should be a unittestcase
-
 class WPML_FormattedPrinter_Test extends WPML_IntegrationTestCase {
+
+    /** @var WPML_FormattedPrinter */
+    private $formattedPrinter;
 
     /** @var $mailServiceMock \No3x\WPML\Model\IMailService|\Mockery\MockInterface */
     private $mailServiceMock;
 
-    /**
-     * Swap ChromeProcess with ChromeProcessMock
-     * so Chrome isn't really launched for each test.
-     */
+
+    private $id = 2;
+
     public function setUp() {
         $this->mailServiceMock = Mockery::mock('No3x\WPML\Model\IMailService');
+
+        /** @var $mail WPML_Mail */
+        $mail = (new WPML_MailExtractor())->extract(WPMailArrayBuilder::aMail()->withSubject("Test")->withTo("example@exmple.com")->withHeaders("From: \"admin\" <admin@local.test>\r\n,\nCc: example2@example.com,\nReply-To: admin <admin@local.test>\r\n")->withMessage("<b>Bold</b><script>alert('xss');</script>")->build());
+        $mail->set_mail_id($this->id);
+        $mail->set_plugin_version('1.8.5');
+        $mail->set_timestamp('2018-09-24 16:02:11');
+        $mail->set_host('127.0.0.1');
+        $mail->set_error('a');
+
+        $this->mailServiceMock->shouldReceive('find_one')
+            ->times(1)
+            ->with( $this->id )
+            ->andReturn( $mail );
+
+        $this->formattedPrinter = new WPML_FormattedPrinter($this->mailServiceMock, ['json', 'html']);
     }
 
     /**
@@ -30,23 +46,7 @@ class WPML_FormattedPrinter_Test extends WPML_IntegrationTestCase {
      * @param $expected string the expected output
      */
     public function test_json($format, $expected) {
-        $printer = new WPML_FormattedPrinter( $this->mailServiceMock, ['json', 'html']);
-        $id = 2;
-
-        /** @var $mail WPML_Mail */
-        $mail = (new WPML_MailExtractor())->extract(WPMailArrayBuilder::aMail()->withSubject("Test")->withTo("example@exmple.com")->withHeaders("From: \"admin\" <admin@local.test>\r\n,\nCc: example2@example.com,\nReply-To: admin <admin@local.test>\r\n")->withMessage("<b>Bold</b>")->build());
-        $mail->set_mail_id($id);
-        $mail->set_plugin_version('1.8.5');
-        $mail->set_timestamp('2018-09-24 16:02:11');
-        $mail->set_host('127.0.0.1');
-
-        $this->mailServiceMock->shouldReceive('find_one')
-            ->times(1)
-            ->with( $id )
-            ->andReturn( $mail );
-
-        $this->assertEquals($expected, $printer->print_email($id, $format));
-
+        $this->assertEquals($expected, $this->formattedPrinter->print_email($this->id, $format));
         $this->mailServiceMock->mockery_verify();
     }
 
@@ -78,5 +78,26 @@ class WPML_FormattedPrinter_Test extends WPML_IntegrationTestCase {
                 '<span class="title">Time: </span>2018-09-24 16:02:11<span class="title">Receiver: </span>example@exmple.com<span class="title">Subject: </span>The subject<span class="title">Message: </span>This is a message<span class="title">Headers: </span><span class="title">Attachments: </span><span class="title">Error: </span>'
             ],
         ];
+    }
+
+    public function test_render_mail() {
+        $expected = '<span class="title">Time: </span>2018-09-24 16:02:11<span class="title">Receiver: </span>example@exmple.com<span class="title">Subject: </span>Test<span class="title">Message: </span>&lt;b&gt;Bold&lt;/b&gt;<span class="title">Headers: </span>From: &quot;admin&quot; ,\nCc: example2@example.com,\nReply-To: admin <span class="title">Attachments: </span><span class="title">Error: </span><i class="fa fa-exclamation-circle" title="a"></i>';
+        $actual = $this->formattedPrinter->print_email($this->id, 'raw');
+        $this->assertContains('2018-09-24 16:02:11', $actual, "The timestamp should be in the rendered mail");
+        $this->assertContains('Test', $actual, "The subject should be in the rendered mail");
+        $this->assertContains('&lt;b&gt;Bold&lt;/b&gt;', $actual, "The rendered mail must have html tags (<b>) escaped");
+        $this->assertNotContains('<script>alert(', $actual, "The rendered mail must strip out evil tags to protect against xss");
+        $this->assertContains('<i class="fa fa-exclamation-circle"', $actual, "The rendered mail has icons for the attachments returned as html, it must not be escaped");
+
+    }
+
+    public function test_render_mail_html() {
+        $expected = '<span class="title">Time: </span>2018-09-24 16:02:11<span class="title">Receiver: </span>example@exmple.com<span class="title">Subject: </span>Test<span class="title">Message: </span><b>Bold</b><span class="title">Headers: </span>From: "admin" ,\nCc: example2@example.com,\nReply-To: admin <span class="title">Attachments: </span><span class="title">Error: </span><i class="fa fa-exclamation-circle" title="a"></i>';
+        $actual = $this->formattedPrinter->print_email($this->id, 'html');
+        $this->assertContains('2018-09-24 16:02:11', $actual, "The timestamp should be in the rendered mail");
+        $this->assertContains('Test', $actual, "The subject should be in the rendered mail");
+        $this->assertContains('<b>Bold</b>', $actual, "The rendered mail must have html tags (<b>) not escaped");
+        $this->assertNotContains('<script>alert(', $actual, "The rendered mail must strip out evil tags to protect against xss");
+        $this->assertContains('<i class="fa fa-exclamation-circle"', $actual, "The rendered mail has icons for the attachments returned as html, it must not be escaped");
     }
 }
