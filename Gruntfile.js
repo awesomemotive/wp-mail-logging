cae154e5a24ae21f619adf455ea9230b293fc47e
Tautvidas Sipavicius <t.9b5as@gmail.com>
2021-11-28 04:15:53 +0200

    Fix redux paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,9 @@ module.exports = function (grunt) {
             },
             clean_build_dependencies: {
                 command: 'rm -R build/lib/vendor/redux-framework/*.json build/lib/vendor/redux-framework/*.lock build/lib/vendor/redux-framework/sample'
+            },
+            copy_redux: {
+                command: 'cp -R lib/vendor/redux-framework build/lib/vendor/redux-framework'
             }
         },
         clean: {
@@ -224,7 +227,7 @@ module.exports = function (grunt) {
     grunt.registerTask('pre_vcs', ['shell:composer', 'version_number', 'copy', 'copyto:vendor', 'compress']);
     grunt.registerTask('do_git', [/*'gitadd',*/ 'gitcommit', 'gittag', 'gitpush']);
 
-    grunt.registerTask('just_build', ['clean_pre_build', 'shell:composer', 'makepot', 'potomo', 'copy', 'copyto:vendor', 'shell:clean_build_dependencies', 'assert-valid-copy', 'compress']);
+    grunt.registerTask('just_build', ['clean_pre_build', 'shell:composer', 'makepot', 'potomo', 'copy', 'copyto:vendor', 'shell:copy_redux','shell:clean_build_dependencies', 'assert-valid-copy', 'compress']);
     grunt.registerTask('release', ['clean_pre_build', 'pre_vcs', 'do_git', 'github-release', 'clean:post_build']);
     grunt.registerTask('compilecss', ['less']);
 };
